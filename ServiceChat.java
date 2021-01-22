dcf6e178e1e5577f71e677915a4e9a404ad935ff
Damien MS-SIS <gounot@et.esiea.fr>
2021-01-22 12:43:26 +0100

    Pseudo Set

--- ServiceChat.java
+++ ServiceChat.java
@@ -3,12 +3,16 @@ import java.net.*;
 
 public class ServiceChat extends Thread {
 
-	final static int NB_USERS_MAX = 3;
+	final static int NB_USERS_MAX = 2;
 	BufferedReader input;
 	PrintStream output;
 	static PrintStream[] outputs = new PrintStream[NB_USERS_MAX];
 	Socket socket;
 	static int nb_users = 0;
+	static String[] usernames = new String[NB_USERS_MAX];
+
+	String username = "";
+	int id_user = 0;
 
 	public ServiceChat(Socket socket) {
 		this.socket = socket;
@@ -46,31 +50,67 @@ public class ServiceChat extends Thread {
 			}
 			return false;
 		}
+		// output.println("DEBUG: rang output = "+nb_users);
+		outputs[nb_users] = output;
+		nb_users++;
+		this.id_user = (nb_users - 1);
 		output.println("Welcome on chat");
-        outputs[nb_users] = output;
-        nb_users ++;
+		output.println("Enter your username: ");
+		this.username = getMessage();
+		// output.println("DEBUG: rang username = "+id_user);
+		usernames[id_user] = this.username;
+		output.println("Welcome on chat mister " + this.username + " !");
+		// output.println("DEBUG: nbUsers = "+nb_users);
+		return true;
+	}
 
-        return true;
-    }
-
-    public synchronized void broadCast(String input){
-		for(int i = 0; i < nb_users; i++ )
-		{
-			try{
-				outputs[i].println(input);
-			}catch(Exception e){
+	public synchronized void broadCast(String input) {
+		// output.println("DEBUG Broadcast: nbUsers = "+nb_users);
+		for (int i = 0; i < nb_users; i++) {
+			try {
+				// output.println("DEBUG Broadcast: send to rang = "+i);
+				outputs[i].println(this.username + ">" + input);
+			} catch (Exception e) {
 				continue;
 			}
 		}
-    }
-    
-    private String getMessage(){
-		String msg="";
+	}
+
+	// public synchronized void sendToUser(String userToSend){
+	// 	for(int i = 0; i < nb_users; i++ )
+	// 	{
+	// 		if()
+	// 		{	
+	// 			try{
+	// 				outputs[i].println(this.username+">"+input);
+	// 			}catch(Exception e){
+	// 				continue;
+	// 			}
+	// 		}
+	// 	}
+    // }
+
+	private String getMessage() {
+		String msg = "";
 		try {
 			msg = input.readLine();
+
 		} catch (IOException e) {
 			output.println("[ERROR] I/O Exception");
 		}
 		return msg;
+	}
+
+	private void analyseMessage(String msg) {
+		switch (msg) {
+			case "/quit":
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			break;
+			default:
+		}
 	}
 }
