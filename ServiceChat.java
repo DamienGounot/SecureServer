a11fe66bd2140f45394bc2a8f3497adbb22e4205
Damien MS-SIS <gounot@et.esiea.fr>
2021-01-25 12:07:33 +0100

    log out
    update outputs, IDs, and nbusers after logout

--- ServiceChat.java
+++ ServiceChat.java
@@ -1,23 +1,28 @@
 import java.io.*;
 import java.net.*;
 
+
 public class ServiceChat extends Thread {
 
-	final static int NB_USERS_MAX = 2;
+
+	final static int NB_USERS_MAX = 3;
 	BufferedReader input;
 	PrintStream output;
 	static PrintStream[] outputs = new PrintStream[NB_USERS_MAX];
 	Socket socket;
 	static int nb_users = 0;
-	static String[] usernames = new String[NB_USERS_MAX];
+	//static String[] usernames = new String[NB_USERS_MAX];
+	static ServiceChat[] serviceChat = new ServiceChat[NB_USERS_MAX];
+	String username;
+	int id_user;
+	boolean loop= true;
 
-	String username = "";
-	int id_user = 0;
 
 	public ServiceChat(Socket socket) {
 		this.socket = socket;
 		this.start();
 	}
+
 
 	public void run() {
 		boolean noError = initStreams();
@@ -25,12 +30,14 @@ public class ServiceChat extends Thread {
 			mainLoop();
 	}
 
+
 	private void mainLoop() {
-		while (true) {
+		while (loop) {
 			String message = getMessage();
-			broadCast(message);
+			analyseMessage(message);
 		}
 	}
+
 
 	private synchronized boolean initStreams() {
 		try {
@@ -49,46 +56,45 @@ public class ServiceChat extends Thread {
 				e.printStackTrace();
 			}
 			return false;
+		}else{
+
+			//output.println("DEBUG: ID output = "+nb_users);
+			this.id_user = nb_users;
+			serviceChat[id_user] = this;
+			outputs[nb_users] = output;
+			nb_users++;
+	
+			output.println("Welcome on chat");
+			output.println("Enter your username: ");
+			this.username = getMessage();
+			
+			// do{
+			// 	output.println("Enter your username: ");
+			// 	this.username = getMessage();
+			// }while(!isUsernameAvailable(this.username));
+	
+			//output.println("DEBUG: ID user = "+id_user);
+
+			//usernames[id_user] = this.username;
+			
+			broadCast("[SYSTEM] " + username + " has join the chat");
+			//output.println("DEBUG: nbUsers = "+nb_users);
+			return true;
 		}
-		// output.println("DEBUG: rang output = "+nb_users);
-		outputs[nb_users] = output;
-		nb_users++;
-		this.id_user = (nb_users - 1);
-		output.println("Welcome on chat");
-		output.println("Enter your username: ");
-		this.username = getMessage();
-		// output.println("DEBUG: rang username = "+id_user);
-		usernames[id_user] = this.username;
-		output.println("Welcome on chat mister " + this.username + " !");
-		// output.println("DEBUG: nbUsers = "+nb_users);
-		return true;
 	}
 
 	public synchronized void broadCast(String input) {
-		// output.println("DEBUG Broadcast: nbUsers = "+nb_users);
+		 //output.println("DEBUG Broadcast: nbUsers = "+nb_users);
 		for (int i = 0; i < nb_users; i++) {
 			try {
-				// output.println("DEBUG Broadcast: send to rang = "+i);
-				outputs[i].println(this.username + ">" + input);
+				 //output.println("DEBUG Broadcast: send to rang = "+i);
+				outputs[i].println(input);
 			} catch (Exception e) {
 				continue;
 			}
 		}
 	}
 
-	// public synchronized void sendToUser(String userToSend){
-	// 	for(int i = 0; i < nb_users; i++ )
-	// 	{
-	// 		if()
-	// 		{	
-	// 			try{
-	// 				outputs[i].println(this.username+">"+input);
-	// 			}catch(Exception e){
-	// 				continue;
-	// 			}
-	// 		}
-	// 	}
-    // }
 
 	private String getMessage() {
 		String msg = "";
@@ -101,16 +107,39 @@ public class ServiceChat extends Thread {
 		return msg;
 	}
 
+
 	private void analyseMessage(String msg) {
 		switch (msg) {
 			case "/quit":
 				try {
+					outputs[id_user] = outputs[nb_users-1];
+					output.println("[DEBUG] outputs["+id_user+"]= outputs["+(nb_users-1)+"]");
+					output.println("[DEBUG] BEFORE UPDATE, UserID: "+serviceChat[(nb_users-1)].id_user);
+					serviceChat[(nb_users-1)].id_user = this.id_user;
+					output.println("[DEBUG] AFTER UPDATE, UserID: "+serviceChat[(nb_users-1)].id_user);
+					nb_users --;
+					output.println("[DEBUG] nbUser ="+nb_users);
+					broadCast( "[SYSTEM] " + username + " has left!" );
 					socket.close();
-				} catch (IOException e) {
+					loop = false;
+				} catch (Exception e) {
 					e.printStackTrace();
+					System.out.println("Erreur /quit");
 				}
-			break;
+				break;
+				
 			default:
+				broadCast("<" + username + "> " + msg);
 		}
 	}
+
+	// 	private boolean isUsernameAvailable(String name){
+	// 		for(int i = 0; i< nb_users; i++){
+	// 			if(usernames[i].equals(name)){
+	// 				output.println("Error: This username is already taken !");
+	// 				return false;
+	// 			}
+	// 		}
+	// 		return true;
+	// 	}
 }
